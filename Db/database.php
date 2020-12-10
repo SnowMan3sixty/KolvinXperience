ef8f038f12d73990eee255b69925207befb59da1
a18paucaltov <a18paucaltov@inspedralbes.cat>
2020-12-10 08:49:13 +0100

    database conection

---
+++ Db/database.php
@@ -0,0 +1,20 @@
+<?php
+
+    $server = "localhost";
+    $user = "a18rubonclop_kx";
+    $pass = "123456";
+    $table = "a18rubonclop_kx";
+
+    $connection = mysql_connect(
+        $server,
+        $user,
+        $pass,
+        $table,
+    )
+
+    if ($connection) {
+        die("Connection failed: " . $connection);
+      }
+      echo "Connected successfully";
+      
+?>
