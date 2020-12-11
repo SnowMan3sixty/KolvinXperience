9934d778b982cbc37126465129616a02eaa70b3a
kevilarriega <a18kevlarpal@inspedralbes.cat>
2020-12-11 11:48:34 +0100

    Index mejorado con popup

--- header.php
+++ header.php
@@ -1,10 +1,51 @@
-<header class="hindex">
-    <div class="logo">
-        <a href="index.php"><img src="logo.png" width="150" height="80"></a>
+<style>
+    .header {
+        padding-top: 5px;
+        padding-bottom: 0px;
+        display: flex;
+        background: #1cc4fb;
+        color: #0038b9;
+        justify-content: space-around;
+    }
+    .header-left {
+        text-align: left;
+    }
+    .header-center {
+        text-align: center;
+        font-size: 50px;
+    }
+    .header-right {
+        text-align: right;
+    }
+
+    h1 {
+    text-align: center;
+    font-family: 'Oswald', Helvetica, sans-serif;
+    font-size: 30px;
+    transform: skewY(-10deg);
+    letter-spacing: 4px;
+    word-spacing: -8px;
+    color: #0038b9;
+    text-shadow: 
+        -1px -1px 0 #00257a,
+        -2px -2px 0 #00257a,
+        -3px -3px 0 #00257a,
+        -4px -4px 0 #00257a,
+        -5px -5px 0 #00257a,
+        -6px -6px 0 #00257a,
+        -7px -7px 0 #00257a,
+        -8px -8px 0 #00257a,
+        -30px 20px 40px blue
+    }
+</style>
+<div class="header">
+    <div class="header-left">
+        <a href = "#top"><img src="logo.png" alt="logo" width="250px" height="150px"></a>
     </div>
-    <nav id="navBar" class="navOpciones">
-        <button class="btn-popup" href="index.php">Inicio</button>
-        <button id="btn-abrir-popup" class="btn-popup">Ir al Login</button>
-        <button id="btn-registrar" class="btn-popup">Registrar</button>
-    </nav>
-</header>
+    <div class="header-center">
+        <h1> Kolvin<br> Xperience</h1>
+    </div>
+    <div class="header-right">
+        <a href = "#top"><img src="https://media.giphy.com/media/2vpauToCoDisB8FzlP/giphy.gif" alt="gif" width="250px" height="150px"></a>
+    </div>
+</div>
