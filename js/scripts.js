4e40102eda212aef87649583cf5a1189247cec64
SnowMan3Sixty <a18rubonclop@inspedralbes.cat>
2020-12-18 08:56:37 +0100

    testing

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function printNoLogged() {
             for(let i = 0; i< resultObj.length; i++){
                 var experiencia = resultObj[i];
 
-                experienciesDiv.html(experienciesDiv.html() + '<div id="experiencia'+i+'" class="ultimesEx"><div class="titleExperiencia">' + experiencia['titol'] + '</div><img class="imgExperiencia" src="' + experiencia['imatge'] +'"></img></div>');
+                experienciesDiv.html(experienciesDiv.html() + '<div id="experiencia'+i+'" class="ultimesEx"><div class="titleExperiencia">' + experiencia['titol'] + '</div><img class="imgExperiencia" src="' + experiencia['imatge'] +'"></img><button id="btn-verExperiencia'+i+'" class="btn-popup">Examinar</button></div>');
             }
 
         }
@@ -84,10 +84,6 @@ $(document).ready(function(){
             }
             
         }
-    });
-    
-    $('#experiencia0').click(function() {
-        console.log("HOLA");
     });
 
     $('#login').click(function() {
